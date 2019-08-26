21918bc354d4dbb090bdaa55bb758f7b2c4aa18e
Hong Baojin <13809735212@163.com>
2019-08-26 18:53:44 +0800

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+$(function () {
+  /*
+  hoverBtn: 鼠标移入移出按钮,
+  showBox: 要显示的内容,
+  flag: 标识 1表示移除，0表示显示
+  bg: 要显示内容背景
+  */
+  function hoverHandle(hoverBtn, showBox, flag, bg) {
+    $(hoverBtn).mouseenter(function () {
+      if (flag) {
+        // 1
+        $(this).addClass('sel'); // 替换背景图片
+        $(bg).show();
+        $(showBox).css({
+          height: 'auto',
+          overflow: 'visible'
+        });
+        flag = !flag; // 更新标识
+      } else {
+        $(this).removeClass('sel');
+        $(bg).hide();
+        $(showBox).css({
+          height: '56px',
+          overflow: 'hidden'
+        });
+        flag = !flag;
+      }
+    });
+  }
+
+  // nav
+  hoverHandle('.dropdown-btn', '.nav ul', 1, '.menu-bg');
+
+  $('.nav ul li').each(function () {
+    $(this).mouseenter(function () {
+      $(this).addClass('active').siblings().removeClass('active');
+    });
+  });
+});
