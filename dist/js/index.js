a2be522b82380f7b8296b08fda9888a0d99c5f57
Hong Baojin <13809735212@163.com>
2019-08-27 18:25:31 +0800

    write to right-suspend

--- dist/js/index.js
+++ dist/js/index.js
@@ -32,10 +32,16 @@ $(function () {
 
   // nav
   hoverHandle('.dropdown-btn', '.nav ul', 1, '.menu-bg');
-
+  // nav
   $('.nav ul li').each(function () {
     $(this).mouseenter(function () {
       $(this).addClass('active').siblings().removeClass('active');
+    });
+  });
+
+  $('.degree .cont-list li').each(function (index) {
+    $(this).mouseenter(function () {
+      $($('.degree .detail-item')[index]).addClass('active').siblings().removeClass('active');
     });
   });
 });
